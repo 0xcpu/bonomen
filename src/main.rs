f9b7c954d82fba8d29176c791ae105424993ca8e
ner0x652 <cornel.punga@gmail.com>
2016-12-22 23:43:40 +0200

    Add banner and basic cmd args processing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
+#[macro_use]
 extern crate clap;
 
 use clap::{Arg, App};
 
+const BONOMEN_BANNER: &'static str = r"
+      =======  ======= ==    == ======= ========== ====== ==    ==
+      ||   //  ||   || ||\\  || ||   || ||\\  //|| ||     ||\\  ||
+      ||====   ||   || || \\ || ||   || ||  ||  || ||==== || \\ ||
+      ||   \\  ||   || ||  \\|| ||   || ||  ||  || ||     ||  \\||
+      =======  ======= ==    == ======= ==  ==  == ====== ==    ==";
+
+const DEFAULT_FILE: &'static str = "default_procs.txt";
+
 fn main() {
-    let matches = App::new("=== BONOMEN ===")
-        .version("0.1")
-        .author("ner0x652 cornel.punga@gmail.com")
+    let matches = App::new(BONOMEN_BANNER)
+        .version(crate_version!())
+        .author(crate_authors!())
         .about("Detect critical process impersonation")
         .arg(Arg::with_name("proc")
              .short("p")
@@ -21,4 +31,13 @@ fn main() {
              .help("File containing critical processes path, threshold, whitelist")
              .takes_value(true))
         .get_matches();
+
+    println!("{}\n\tAuthor(s):{} Version:{}\n",
+             BONOMEN_BANNER, crate_authors!(), crate_version!());
+    
+    let proc_name = matches.value_of("proc").unwrap();
+    let file_name = matches.value_of("file").unwrap_or(DEFAULT_FILE);
+
+    println!("Testing process with name: {}", proc_name);
+    println!("Standard processes file: {}", file_name);
 }
