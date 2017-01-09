80bf91c48627d10281dca2815ee614c3b70c9f58
ner0x652 <cornel.punga@gmail.com>
2017-01-09 23:18:29 +0200

    Add process properties structure. Read processes from file into hash map

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,15 @@ extern crate clap;
 
 use clap::{Arg, App};
 
+mod types;
+
+use std::error::Error;
+use std::fs::File;
+use std::path::Path;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::collections::HashMap;
+
 const BONOMEN_BANNER: &'static str = r"
       =======  ======= ==    == ======= ========== ====== ==    ==
       ||   //  ||   || ||\\  || ||   || ||\\  //|| ||     ||\\  ||
@@ -40,4 +49,42 @@ fn main() {
 
     println!("Testing process with name: {}", proc_name);
     println!("Standard processes file: {}", file_name);
+
+    let crit_proc_hm = read_procs_file(&file_name);
+    for (pn, prop) in crit_proc_hm {
+        println!("{} : {}", pn, prop.threshold);
+        for wle in prop.whitelist.iter() {
+            println!("{}", wle);
+        }
+    }
+}
+
+fn read_procs_file(file_name: &str) -> HashMap<std::string::String, types::ProcProps> {
+    let path    = Path::new(file_name);
+    let display = path.display();
+
+    let file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why.description()),
+        Ok(file) => file,
+    };
+    
+    let mut hash_map = HashMap::new();
+
+    let reader = BufReader::new(file);
+    let lines  = reader.lines().map(|l| l.unwrap());
+
+    for line in lines {
+        let v: Vec<_> = line.split(';').map(|s| s.to_string()).collect();
+        let mut wl    = Vec::new();
+
+        for i in 2 .. v.len() {
+            wl.push(v[i].to_string());
+        }
+        
+        hash_map.insert(v[0].to_string(), types::ProcProps{ threshold: v[1].parse::<u32>().unwrap(),
+                                                whitelist: wl
+        });
+    }
+
+    hash_map
 }
---
+++ src/types.rs
@@ -0,0 +1,4 @@
+pub struct ProcProps {
+    pub threshold: u32,
+    pub whitelist: Vec<::std::string::String>,
+}
